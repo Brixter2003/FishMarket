25c1de65dfdccee6d408ffd7c04159f89d0ab769
Brixter2003 <87116239+Brixter2003@users.noreply.github.com>
2024-10-01 12:14:19 -0700

    Add files via upload

---
+++ fishMarketMenu.js
@@ -0,0 +1,308 @@
+
+
+
+     var RSLunch = "3PC RED SNAPPER";
+        var RSLunch1 = "3PC CATFISH FILLET";
+        var RSLunch2 = '3PC SOLLE FILLET';
+        var RSLunch3 = '3PC CAT STEAK';
+        var RSLunch4 = '3PC SANDAB';
+        var RSLunch5 = '3PC TILAPIA';
+        var RSLunch6 = '3PC WHITTING';
+        var lunchPrice = '10.99';
+        var pick1Side = "PICK ONE SIDE";
+        // var lunchPrice1 = '11.99';
+        // var RSLunch2 = "3PC Solle Fillet";
+        // var foodChoice = document.querySelector('a');
+        // var foodChoice = document.getElementsByClassName('infoMenuDiv');
+        // var foodChosen = document.getElementsByClassName('modalHeader');
+        //  var collection = document.getElementsByName(''); */}
+
+        function pickedFood(){
+            document.getElementById('output1').innerHTML = RSLunch;
+            document.getElementById('output2').innerHTML = lunchPrice;
+            document.getElementById('output3').innerHTML = pick1Side;
+        }
+
+        function pickedFood2(){
+            document.getElementById('output1').innerHTML = RSLunch1;
+            document.getElementById('output2').innerHTML = lunchPrice;
+            document.getElementById('output3').innerHTML = pick1Side;
+        }
+        function pickedFood3(){
+            document.getElementById('output1').innerHTML = RSLunch2;
+            document.getElementById('output2').innerHTML = lunchPrice;
+            document.getElementById('output3').innerHTML = pick1Side;
+        }
+        function pickedFood4(){
+            document.getElementById('output1').innerHTML = RSLunch3;
+            document.getElementById('output2').innerHTML = lunchPrice;
+            document.getElementById('output3').innerHTML = pick1Side;
+        }
+        function pickedFood5(){
+            document.getElementById('output1').innerHTML = RSLunch4;
+            document.getElementById('output2').innerHTML = lunchPrice;
+            document.getElementById('output3').innerHTML = pick1Side;
+        }
+        function pickedFood6(){
+            document.getElementById('output1').innerHTML = RSLunch5;
+            document.getElementById('output2').innerHTML = lunchPrice;
+            document.getElementById('output3').innerHTML = pick1Side;
+        }
+        function pickedFood7(){
+            document.getElementById('output1').innerHTML = RSLunch6;
+            document.getElementById('output2').innerHTML = lunchPrice;
+            document.getElementById('output3').innerHTML = pick1Side;
+            
+        }
+         // combo lunch
+        var ComboLunch = '2PC + 6 MD SHRIMP COMBO';
+        var ComboLunch1 = '3PC + 10 MD SHRIMP COMBO';
+        var ComboLunch2 = '4PC + 13 MD SHRIMP COMBO'; 
+        var lunchCombo1 = '12.99';
+        var lunchCombo2 = '17.99';
+        var lunchCombo3 = '22.99';
+
+        function pickedFood8(){
+            document.getElementById('output1').innerHTML = ComboLunch;
+            document.getElementById('output2').innerHTML = lunchCombo1;
+            document.getElementById('output3').innerHTML = pick1Side;
+        }
+        function pickedFood9(){
+            document.getElementById('output1').innerHTML = ComboLunch1;
+            document.getElementById('output2').innerHTML = lunchCombo2;
+            document.getElementById('output3').innerHTML = pick1Side;
+        }
+        function pickedFood10(){
+            document.getElementById('output1').innerHTML = ComboLunch2;
+            document.getElementById('output2').innerHTML = lunchCombo3;
+            document.getElementById('output3').innerHTML = pick1Side;
+        }
+        //  dinner lines
+
+        var Dinner = "5PC RED SNAPPER";
+        var Dinner1 = "7PC RED SNAPPER";
+        var Dinner2 = "10PC RED SNAPPER";
+        var Dinner3 = "5PC CAT FILLET";
+        var Dinner4 = "7PC CAT FILLET";
+        var Dinner5 = "10PC CAT FILLET";
+        var Dinner6 = "5PC SOLLE FILLET";
+        var Dinner7 = "5PC SANDAB FILLET";
+        var Dinner8 = "5PC TILAPIA FILLET";
+        var Dinner9 = "5PC WHITTING FILLET";
+        var Dinner10 = "5PC CAT STEAK FILLET";
+        var Dinner11 = "PERCH DINNER";
+        var Dinner12 = "PAC OYSTER DINNER";
+        var Dinner13 = "SCALLOP DINNER";
+        var Dinner14 = "L.A. OYSTER DINNER";
+
+        var dinnerPrice1 = "14.99";
+        var dinnerPrice2 = "18.99";
+        var dinnerPrice3 = "24.99";
+        var dinnerPrice4 = "15.99";
+        var dinnerPrice5 = "19.99";
+        var dinnerPrice6 = "25.99";
+        var dinnerPrice7 = "14.99";
+        var dinnerPrice8 = "14.99";
+        var dinnerPrice9 = "18.99";
+        var dinnerPrice10 = "14.99";
+        var dinnerPrice11 = "15.99";
+        var dinnerPrice12 = "14.99";
+        var dinnerPrice13 = "14.99";
+        var dinnerPrice14 = "15.99";
+        var dinnerPrice15 = "17.99";
+
+        function pickedDinner(){
+            document.getElementById('output1').innerHTML = Dinner;
+            document.getElementById('output2').innerHTML = dinnerPrice1;
+            document.getElementById('output3').innerHTML = pick2Sides;
+        }
+        function pickedDinner1(){
+            document.getElementById('output1').innerHTML = Dinner1;
+            document.getElementById('output2').innerHTML = dinnerPrice2;
+            document.getElementById('output3').innerHTML = pick2Sides;
+        }
+        function pickedDinner2(){
+            document.getElementById('output1').innerHTML = Dinner2;
+            document.getElementById('output2').innerHTML = dinnerPrice3;
+            document.getElementById('output3').innerHTML = pick2Sides;
+        }
+        function pickedDinner3(){
+            document.getElementById('output1').innerHTML = Dinner3;
+            document.getElementById('output2').innerHTML = dinnerPrice4;
+            document.getElementById('output3').innerHTML = pick2Sides;
+        }
+        function pickedDinner4(){
+            document.getElementById('output1').innerHTML = Dinner4;
+            document.getElementById('output2').innerHTML = dinnerPrice5;
+            document.getElementById('output3').innerHTML = pick2Sides;
+        }
+        function pickedDinner5(){
+            document.getElementById('output1').innerHTML = Dinner5;
+            document.getElementById('output2').innerHTML = dinnerPrice6;
+            document.getElementById('output3').innerHTML = pick2Sides;
+        }
+        function pickedDinner6(){
+            document.getElementById('output1').innerHTML = Dinner6;
+            document.getElementById('output2').innerHTML = dinnerPrice7;
+            document.getElementById('output3').innerHTML = pick2Sides;
+        }
+        function pickedDinner7(){
+            document.getElementById('output1').innerHTML = Dinner7;
+            document.getElementById('output2').innerHTML = dinnerPrice8;
+            document.getElementById('output3').innerHTML = pick2Sides;
+        }
+        function pickedDinner8(){
+            document.getElementById('output1').innerHTML = Dinner8;
+            document.getElementById('output2').innerHTML = dinnerPrice9;
+            document.getElementById('output3').innerHTML = pick2Sides;
+        }
+        function pickedDinner9(){
+            document.getElementById('output1').innerHTML = Dinner9;
+            document.getElementById('output2').innerHTML = dinnerPrice10;
+            document.getElementById('output3').innerHTML = pick2Sides;
+        }
+        function pickedDinner10(){
+            document.getElementById('output1').innerHTML = Dinner10;
+            document.getElementById('output2').innerHTML = dinnerPrice11;
+            document.getElementById('output3').innerHTML = pick2Sides;
+        }
+        function pickedDinner11(){
+            document.getElementById('output1').innerHTML = Dinner11;
+            document.getElementById('output2').innerHTML = dinnerPrice12;
+            document.getElementById('output3').innerHTML = pick2Sides;
+        }
+        function pickedDinner12(){
+            document.getElementById('output1').innerHTML = Dinner12;
+            document.getElementById('output2').innerHTML = dinnerPrice13;
+            document.getElementById('output3').innerHTML = pick2Sides;
+        }
+        function pickedDinner13(){
+            document.getElementById('output1').innerHTML = Dinner13;
+            document.getElementById('output2').innerHTML = dinnerPrice14;
+            document.getElementById('output3').innerHTML = pick2Sides;
+        }
+        function pickedDinner14(){
+            document.getElementById('output1').innerHTML = Dinner14;
+            document.getElementById('output2').innerHTML = dinnerPrice15;
+            document.getElementById('output3').innerHTML = pick2Sides;
+        }
+    //    combo Dinner
+        var comboD1 = "2PC FISH + 2PC SHRIMP";
+        var comboD2 = "2PC FISH + 2PC SHRIMP +2 OYSTERS";
+        var comboD3 = "3PC FISH + 3PC SHRIMP";
+        var comboD4 = "3PC FISH + 3PC SHRIMP + 3 OYSTERS";
+        var comboD5 = "4PC FISH + 4PC SHRIMP";
+        var comboD6 = "5PC FISH + 5PC SHRIMP";
+        var comboD7 = "8PC FISH + 8PC SHRIMP";
+        var comboD8 = "10PC FISH + 10PC SHRIMP";
+        var comboD9 = "OYSTER COMBO"; 
+        var dinnerCombo1 = "11.99";
+        var dinnerCombo2 = "13.99";
+        var dinnerCombo3 = "13.99";
+        var dinnerCombo4 = "20.99";
+        var dinnerCombo5 = "19.99";
+        var dinnerCombo6 = "20.99";
+        var dinnerCombo7 = "35.99";
+        var dinnerCombo8 = "40.99";
+        var dinnerCombo9 = "23.99";
+        var pick2Sides = "PICK TWO SIDES";
+
+        function comboDinner1(){
+            document.getElementById('output1').innerHTML = comboD1;
+            document.getElementById('output2').innerHTML = dinnerCombo1;
+            document.getElementById('output3').innerHTML = pick2Sides;
+        }
+        function comboDinner2(){
+            document.getElementById('output1').innerHTML = comboD2;
+            document.getElementById('output2').innerHTML = dinnerCombo2;
+            document.getElementById('output3').innerHTML = pick2Sides;
+        }
+        function comboDinner3(){
+            document.getElementById('output1').innerHTML = comboD3;
+            document.getElementById('output2').innerHTML = dinnerCombo2;
+            document.getElementById('output3').innerHTML = pick2Sides;
+        }
+        function comboDinner4(){
+            document.getElementById('output1').innerHTML = comboD4;
+            document.getElementById('output2').innerHTML = dinnerCombo4;
+            document.getElementById('output3').innerHTML = pick2Sides;
+        }
+        function comboDinner5(){
+            document.getElementById('output1').innerHTML = comboD5;
+            document.getElementById('output2').innerHTML = dinnerCombo5;
+            document.getElementById('output3').innerHTML = pick2Sides;
+        }
+        function comboDinner6(){
+            document.getElementById('output1').innerHTML = comboD6;
+            document.getElementById('output2').innerHTML = dinnerCombo6;
+            document.getElementById('output3').innerHTML = pick2Sides;
+        }
+        function comboDinner7(){
+            document.getElementById('output1').innerHTML = comboD7;
+            document.getElementById('output2').innerHTML = dinnerCombo7;
+            document.getElementById('output3').innerHTML = pick2Sides;
+        }
+        function comboDinner8(){
+            document.getElementById('output1').innerHTML = comboD8;
+            document.getElementById('output2').innerHTML = dinnerCombo8;
+            document.getElementById('output3').innerHTML = pick2Sides;
+        }
+        function comboDinner9(){
+            document.getElementById('output1').innerHTML = comboD9;
+            document.getElementById('output2').innerHTML = dinnerCombo9;
+            document.getElementById('output3').innerHTML = pick2Sides;
+        }
+        // shrimps line
+        var shrimpOrder1 = "10 SHRIMPS WITH FRIES";
+        var shrimpOrder2 = "15 SHRIMPS WITH FRIES";
+        var shrimpOrder3 = "25 SHRIMPS WITH FRIES";
+        var shrimpOrder4 = "50  SHRIMPS WITH FRIES";
+        
+        function shrimpCombo1(){
+            document.getElementById('output1').innerHTML = shrimpOrder1;
+        }
+        function shrimpCombo2(){
+            document.getElementById('output1').innerHTML = shrimpOrder2;
+        }
+        function shrimpCombo3(){
+            document.getElementById('output1').innerHTML = shrimpOrder3;
+        }
+        function shrimpCombo4(){
+            document.getElementById('output1').innerHTML = shrimpOrder4;
+        }
+
+        // shrimp only
+        var shrimpy1 = "10 MED SHRIMP ONLY";
+        var shrimpy2 = "20 MED SHRIMP ONLY";
+        var shrimpy3 = "30 MED SHRIMP ONLY";
+        var shrimpy4 = "40 MED SHRIMP ONLY"; 
+
+        function shrimpOnly1(){
+            document.getElementById('output1').innerHTML = shrimpy1;
+        }
+        function shrimpOnly2(){
+            document.getElementById('output1').innerHTML = shrimpy2;
+        }
+        function shrimpOnly3(){
+            document.getElementById('output1').innerHTML = shrimpy3;
+        }
+        function shrimpOnly4(){
+            document.getElementById('output1').innerHTML = shrimpy4;
+        }
+         // JUMBO SHRIMPS 
+
+        var JBshrimpy1 = "10PC JUMBO SHRIMP WITH FRIES";
+        var JBshrimpy2 = "16PC JUMBO SHRIMP WITH FRIES";
+        var JBshrimpy3 = "20PC JUMBO SHRIMP WITH FRIES"; 
+
+        function jumboShrimp1(){
+            document.getElementById('output1').innerHTML = JBshrimpy1;
+        }
+        function jumboShrimp2(){
+            document.getElementById('output1').innerHTML = JBshrimpy2;
+        }
+        function jumboShrimp3(){
+            document.getElementById('output1').innerHTML = JBshrimpy3;
+        }
+        
+  
